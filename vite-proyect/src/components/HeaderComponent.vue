ffd88dc67cfbc18ff1656a5590dfa6aca63ca20c
LucianoHerrera123 <lucianoherreraim98@gmail.com>
2026-04-07 14:24:28 -0300

    clase 4

---
+++ vite-proyect/src/components/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<script></script>
+<template>
+<header>
+<div>
+    <a href="#" aria-label="Messi y futbol, inicio">
+        <span aria-hidden="true">🐏</span>
+        <span> Messi & futbol </span>
+        <span>Buen futbol para la gente</span>
+    </a>
+    <nav>
+        <ul>
+            <li>Aca va a estar el nav</li>
+            
+        </ul>
+    </nav>
+</div>
+
+
+</header>
+
+
+
+</template>
+<style></style>
